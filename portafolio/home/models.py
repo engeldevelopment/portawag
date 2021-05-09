47028caa880f7a6340dbe764a84a3458a767c1be
Engel Pinto <engeljavierpinto@gmail.com>
2021-05-09 17:20:40 -0400

    feat: Agregué la configuracion SocialMediaSetting.
    Ahora se agrega la configuración de las redes sociales
    de forma dinámica.

--- portafolio/home/models.py
+++ portafolio/home/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from wagtail.contrib.settings.models import BaseSetting
+from wagtail.contrib.settings.registry import register_setting
 
 from wagtail.core.models import Page
 from wagtail.admin import edit_handlers
@@ -31,3 +33,43 @@ class HomePage(Page):
         ], "Información Básica"),
         ImageChooserPanel('photo', 'Foto de Perfil')
     ]
+
+
+@register_setting(icon="placeholder")
+class SocialMediaSetting(BaseSetting):
+    facebook = models.URLField(
+        help_text="Link a tu cuenta de Facebook",
+        blank=True,
+        null=True
+    )
+    github = models.URLField(
+        help_text="Link a tu cuenta de GitHub",
+        blank=True,
+        null=True
+    )
+    gitlab = models.URLField(
+        help_text="Link a tu cuenta de GitLab",
+        blank=True,
+        null=True
+    )
+    linkedin = models.URLField(
+        help_text="Link a tu cuenta de Linkedin",
+        blank=True,
+        null=True
+    )
+    instagram = models.URLField(
+        help_text="Link a tu cuenta de Instagram",
+        blank=True,
+        null=True
+    )
+
+    panels = [
+        edit_handlers.FieldPanel('facebook'),
+        edit_handlers.FieldPanel('github'),
+        edit_handlers.FieldPanel('gitlab'),
+        edit_handlers.FieldPanel('linkedin'),
+        edit_handlers.FieldPanel('instagram'),
+    ]
+
+    class Meta:
+        verbose_name = "Configuración de Redes Sociales"
